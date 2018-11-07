f8cda3853110acb59a0b7ec5f2c6856566194ac1
rozina85 <43050820+rozina85@users.noreply.github.com>
2018-11-06 22:29:28 -0500

    Update R Script2.R

--- R Script2.R
+++ R Script2.R
@@ -1,3 +1,9 @@
-install.packages("qcc")
-library('qcc')
-pareto.chart(schools_report_progress$`2012-2013 OVERALL SCORE`)
+> pareto.chart(table(schools_report_progress$`2012-2013 OVERALL GRADE`))
+   
+Pareto chart analysis for table(schools_report_progress$`2012-2013 OVERALL GRADE`)
+       Frequency    Cum.Freq.   Percentage Cum.Percent.
+  B 5.810000e+02 5.810000e+02 3.573186e+01 3.573186e+01
+  C 4.560000e+02 1.037000e+03 2.804428e+01 6.377614e+01
+  A 4.470000e+02 1.484000e+03 2.749077e+01 9.126691e+01
+  D 9.600000e+01 1.580000e+03 5.904059e+00 9.717097e+01
+  F 4.500000e+01 1.625000e+03 2.767528e+00 9.993850e+01
