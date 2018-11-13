a7a0950045954eb36d60c3c214797a24e90aacea
Vladimir Prenga <vladimirprenga@Vladimirs-MBP.lehman.edu>
2018-11-13 18:55:43 -0500

    Updated R script

---
+++ R_Script.R
@@ -0,0 +1,21 @@
+dfschool<-read.csv(file.path("/Users/vladimirprenga/Desktop/schools\ report\ progress.csv"))
+dfschool[dfschool$X2012.2013.OVERALL.SCORE=='.',] <-"N/A"
+dfschool[dfschool$X2012.2013.OVERALL.SCORE=='.',] <-NA
+
+skewness(school$X2012.2013.PROGRESS.CATEGORY.SCORE,na.rm = TRUE)
+kurtosis(school$X2012.2013.PROGRESS.CATEGORY.SCORE,na.rm = TRUE)
+mean(school$X2012.2013.OVERALL.SCORE,na.rm = TRUE)
+mean(school$X2012.13.OVERALL.PERCENTILE,na.rm = TRUE)
+mean(school$X2012.2013.PROGRESS.CATEGORY.SCORE,na.rm = TRUE)
+var(school$X2012.2013.OVERALL.SCORE,na.rm = TRUE)
+var(school$X2012.13.OVERALL.PERCENTILE,na.rm = TRUE)
+var(school$X2012.2013.PROGRESS.CATEGORY.SCORE,na.rm = TRUE)
+
+hist(school$X2012.2013.PROGRESS.CATEGORY.SCORE,xlab = "scores")
+pareto.chart(table(schools_report_progress$`2012-2013 OVERALL GRADE`))
+
+t.test(as.integer(dfschool$X2012.2013.OVERALL.SCORE))t.test(as.integer(dfschool$X2012.2013.PROGRESS.CATEGORY.SCORE))
+t.test(as.integer(dfschool$X2012.2013.PROGRESS.CATEGORY.SCORE))
+
+
+
