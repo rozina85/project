605d755472031420667e7dd0be85f7e31dc4376d
Vladimir Prenga <vladimirprenga@Vladimirs-MBP.lehman.edu>
2018-12-11 18:05:47 -0500

    update final Rcode

--- R_Script.R
+++ R_Script.R
@@ -18,11 +18,18 @@ pareto.chart(table(schools_report_progress$`2012-2013 OVERALL GRADE`))
 t.test(as.integer(dfschool$X2012.2013.OVERALL.SCORE))t.test(as.integer(dfschool$X2012.2013.PROGRESS.CATEGORY.SCORE))
 t.test(as.integer(dfschool$X2012.2013.PROGRESS.CATEGORY.SCORE))
 t.test(as.integer(dfschool$X2012.2013.PERFORMANCE.CATEGORY.SCORE))
-
-t.test(as.integer(dfschool$X2012.2013.PROGRESS.CATEGORY.SCORE),as.integer(dfschool$X2012.2013.OVERALL.PERCENTILE),alternative="two.sided")
-
 t.test(as.integer(dfschool$X2012.2013.PERFORMANCE.CATEGORY.SCORE),as.integer(dfschool$X2012.2013.OVERALL.SCORE),alternative="two.sided")
 t.test(as.integer(dfschool$X2012.2013.ENVIRONMENT.CATEGORY.SCORE),as.integer(dfschool$X2012.2013.OVERALL.SCORE),alternative="two.sided")
 
+dfschool.lm<-lm(X2012.2013.PERFORMANCE.CATEGORY.SCORE ~ X2012.2013.OVERALL.SCORE, data=dfschool)
+summary(dfschool.lm)
+plot(X2012.2013.PERFORMANCE.CATEGORY.SCORE ~ X2012.2013.OVERALL.SCORE, data=dfschool)
+abline(coef(dfschool.lm))
 
+grades.aov<-aov(X2012.2013.PERFORMANCE.CATEGORY.SCORE~X2012.2013.PROGRESS.GRADE, data=dfschool)
+summary(grades.aov)
+model.tables(grades.aov,"means")
+posthoc<-TukeyHSD(grades.aov)
+posthoc
+plot(posthoc)
 
